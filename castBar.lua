25b0fec8e84d5381260559f5ace89aae12967a6c
Goranaws <goranaws@arenajunkies.com>
2015-04-21 20:39:21 -0400

    upgrade to ShareMedia support
    Rewrote the texture selector panel to be universal for better SharedMedia support. Corrected template name.

--- castBar.lua
+++ castBar.lua
@@ -5,6 +5,8 @@
 local Addon = _G["Dominos"]
 local Frame = Addon:CreateClass('Frame', Addon.Frame)
 local L = LibStub('AceLocale-3.0'):GetLocale('Dominos-CastingBar')
+local SML = LibStub('LibSharedMedia-3.0')
+
 
 --[[ Bar ]]--
 function Frame:New()
@@ -35,7 +37,7 @@ end
 function Frame:Create(...)
 	local bar = Frame.proto.Create(self, ...)
 	
-	bar.cast = CreateFrame("StatusBar",  bar:GetName().."Bar", bar, "DominosCastingBarFrameTemplate")
+	bar.cast = CreateFrame("StatusBar",  bar:GetName().."Bar", bar, "CastingBarFrameTemplate")
 	bar.cast:SetPoint("Center")
 	bar.cast.unit = "player"
 	bar.cast:SetAttribute("unit", "player")
@@ -48,8 +50,11 @@ function Frame:Create(...)
 	bar.cast.time:SetAllPoints(bar.cast)
 
 	--background handler
+		--now doubles as an anchor
+		--for the castBar and Icon.
 	bar.skin = CreateFrame("Frame", bar:GetName().."Skin", bar.cast)
 	bar.skin:SetFrameLevel(bar:GetFrameLevel()-1)
+	bar.skin:SetPoint("Center", bar)
 
 	bar.cast:HookScript("OnUpdate", function()
 		local _, _, _, _, startTime, endTime = UnitCastingInfo("player")
@@ -92,6 +97,7 @@ function Frame:GetDefaults()
 		width = 20.0,
 		padding = 3,
 		hideText = false,
+		showIcon = true,
 		inset = 3,
 		color = {
 			r = 0,
@@ -104,16 +110,27 @@ function Frame:GetDefaults()
 		alignText = "LEFT",
 		alignTime = "RIGHT",
 		texture = "Interface\\TargetingFrame\\UI-StatusBar",
+		font = "Arial Narrow",
 	}
 end
 
-local Version = 2
+local Version, checkSettings, requiresReset = 2.2, true, false
+
+function Frame:CheckVersion()
+	if ((not self.sets.version) or (self.sets.version ~= Version))then
+		if requiresReset then
+			wipe(self.sets)
+		end
+		self.sets.version = Version
+		if checkSettings then
+			self.sets = check(self:GetDefaults(), self.sets)
+		end
+		self.verified = true
+	end
+end
 
 function Frame:Layout()
-	if (not self.sets.version) or (self.sets.version ~= Version) then
-		wipe(self.sets)
-		self.sets.version = Version
-	end
+	self:CheckVersion()
 
 	if self.sets.hideText then
 		self.cast.text:SetAlpha(0)
@@ -121,43 +138,51 @@ function Frame:Layout()
 		self.cast.text:SetAlpha(1)
 	end
 
-	self.sets = check(self:GetDefaults(), self.sets)
+	self:ToggleIcon()
 	self:Resize()
 	self:Skin()
 	self:ToggleTime()
-	
+
 	self.cast.text:SetJustifyH(self.sets.alignText)
 	self.cast.time:SetJustifyH(self.sets.alignTime)
-	self:UpdateTexture()
+	self:ApplyTexture()
+	local SML = LibStub('LibSharedMedia-3.0', true)
+
+	self.cast.text:SetFont(SML:Fetch('font', self.sets.font), 12)
+	self.cast.time:SetFont(SML:Fetch('font', self.sets.font), 12)
+
 end
 
 function Frame:Resize()
 	local pw, ph = self:GetPadding()
 	local w, h = self.sets.width, self.sets.height
-	self:SetSize((w * 10) + (pw), (h * 10) + (ph))
-
-	local offset = -19
-
+	self:SetSize(w + pw, h + ph)
+	self.skin:SetSize(w, h)
+	local offset = 0
 	if self.sets.showIcon then
-		offset = 0
+		offset = 19
 	end
+	self.cast:SetSize(w-offset, h)
+	local point = "Right"
+	if self.sets.isRightToLeft then 
+		point = "Left"
+	end
+	self.cast:ClearAllPoints()
+	self.cast:SetPoint(point, self.skin)
+end
 
-	self.cast.icon:ClearAllPoints()
-
-	if  self.sets.isRightToLeft then
-		self.cast.icon:SetPoint("Left", self.cast, "Right", 3, 0)
-		self.cast:SetPoint("Left", self, "Center",  -(((w*10)/2)), 0)
-		self.cast:SetPoint("Right", self, "Center", (((w*10)/2) + offset),0)
+function Frame:ToggleIcon()
+	if self.sets.showIcon then
+		self.cast.icon:Show()
+		local point = "Left"
+		if self.sets.isRightToLeft then 
+			point = "Right"
+		end
+		self.cast.icon:ClearAllPoints()
+		self.cast.icon:SetPoint(point, self.skin)
 	else
-		self.cast.icon:SetPoint("Right", self.cast, "Left", -3, 0)
-		self.cast:SetPoint("Left", self, "Center",  -(((w*10)/2) + offset), 0)
-		self.cast:SetPoint("Right", self, "Center", (((w*10)/2)),0)
+		self.cast.icon:Hide()
 	end
-	
-	self.skin:SetPoint("Left", self, "Center",  -(w*5), 0)
-	self.skin:SetPoint("Right", self, "Center", w*5,0)
-	self.skin:SetHeight(h * 10)
-	self.cast:SetHeight(h * 10)
 end
 
 function Frame:Skin()
@@ -187,15 +212,36 @@ function Frame:GetLeftToRight()
     return not self.sets.isRightToLeft
 end
 
-function Frame:UpdateTexture()
-	local LSM = LibStub('LibSharedMedia-3.0', true)
-	local texture = (LSM and LSM:Fetch('statusbar', self.sets.texture)) or DEFAULT_STATUSBAR_TEXTURE
+function Frame:ApplyTexture()
+	local texture = (SML and SML:Fetch('statusbar', self.sets.texture)) or DEFAULT_STATUSBAR_TEXTURE
 	self.cast:SetStatusBarTexture(texture)
 end
 
 function Frame:SetTexture(texture)
 	self.sets.texture = texture
-	self:UpdateTexture()
+	self:ApplyTexture()
+end
+
+function Frame:GetBarTexture()
+	return self.sets.texture
+end
+
+function Frame:SetBackground(texture)
+	self.sets.bgFile = texture
+	self:Skin()
+end
+
+function Frame:GetBackground()
+	return self.sets.bgFile
+end
+
+function Frame:GetFont()
+	return self.sets.font
+end
+
+function Frame:SetFont(font)
+	self.sets.font = font
+	self:Layout()
 end
 
 --[[ Menu Code ]]--
@@ -203,23 +249,23 @@ function NewWidthSlider(menu)
 	return menu:NewSlider("Width", 16, 600, 1,
 		function(self)
 			local width = menu.owner.sets.width
-			self:SetValue(width*10)
+			self:SetValue(width)
 		end, 
 		function(self, value)
-			menu.owner.sets.width = value/10
+			menu.owner.sets.width = value
 			menu.owner:Layout()
 		end
 	)
 end
 
 function NewHeightSlider(menu)
-	return menu:NewSlider("Height", 16, 100, 1,
+	return menu:NewSlider("Height", 5, 100, 1,
 		function(self)
 			local height = menu.owner.sets.height
-			self:SetValue(height*10)
+			self:SetValue(height)
 		end, 
 		function(self, value)
-			menu.owner.sets.height= value/10
+			menu.owner.sets.height= value
 			menu.owner:Layout()
 		end
 	)
@@ -333,6 +379,8 @@ local function NewMenu(menu, name, key, table)
 	local title = f:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
 	title:SetPoint("TopLeft", f.button, "TopRight", 4, -2)
 	title:SetText(name)	
+	
+
 
 	f:SetScript('OnShow', function(self)
 		if self ~= f then
@@ -340,12 +388,17 @@ local function NewMenu(menu, name, key, table)
 		end
 		f:initialize()
 		f.text:SetText(f:GetParent().owner.sets[key])
+		if not self.stretch then
+			self.stretch = 1
+			f.button:SetHitRectInsets(0, -(title:GetCenter()+ (title:GetWidth()/2)- f.button:GetCenter()+ (f.button:GetWidth()/2)), 0, 0)
+		end
 	end)
 
 	f:SetScript("OnHide", function() CloseDropDownMenus() end)
 
 	f.button:SetScript("OnClick", function(self)
-		ToggleDropDownMenu(1, nil, f, "cursor")
+		UIDropDownMenu_SetAnchor(f, -4, 6, "TopLeft", f.button, "BottomLeft")
+		ToggleDropDownMenu(1, nil, f)
 		PlaySound("igMainMenuOptionCheckBoxOn")
 	end)
 
@@ -379,76 +432,86 @@ local function NewMenu(menu, name, key, table)
 	f.point = {f:GetPoint()}
 	menu.checkbutton = f.button
 
+	f.button:SetHitRectInsets(0, 0 , 0, 0)
+
 	menu.height = menu.height + 24
 	return f
 end
 
 
---[[Texture Panel   I plan to do more with this.
-                    I want easy SharedMedia support for:
-                       -fonts
-                       -borders
-                       -statusbars
-                       -backgrounds
-          ~Goranaws
---]]
-local NUM_ITEMS, WIDTH, HEIGHT, OFFSET = 8, 155, 20, 0
-
-local function TextureButton_OnClick(self)
-	self:GetParent().owner:SetTexture(self:GetText())
-	self:GetParent():UpdateList()
+local function SetSelcterInfo(button, mType, filePath, fileName)
+	button:SetText(fileName)
+	if mType == "border" then
+		button:SetBackdrop({
+			edgeFile = filePath,
+			edgeSize = 16,
+		})
+	elseif (mType == "background") or (mType == "statusbar") then
+		button:SetBackdrop({
+			bgFile = filePath,
+			tile = false,
+		})
+	end
+	if mType == "font" then
+		button:GetFontString():SetFont(filePath, 12)
+		button:SetBackdrop(nil)
+	else
+		button:GetFontString():SetFont(SML:Fetch("font", "GameFontNormal"), 12)
+		local r, g, b = max(random(), 0.2), max(random(), 0.2), max(random(), 0.2)
+		button:SetBackdropColor(r, g, b)
+		button:SetBackdropBorderColor(r, g, b)
+	end
+	button:GetFontString():SetAllPoints(button)
+	button:Show()
 end
 
-local function TextureButton_OnMouseWheel(self, direction)
-	local scrollBar = _G[self:GetParent().scroll:GetName() .. 'ScrollBar']
-	scrollBar:SetValue(scrollBar:GetValue() - direction * (scrollBar:GetHeight()/2))
-	parent:UpdateList()
-end
-
-local function TextureButton_Create(name, parent)
-	local button = CreateFrame('Button', name, parent)
-	button:SetWidth(WIDTH)
-	button:SetHeight(HEIGHT)
-
-	button.bg = button:CreateTexture()
-	button.bg:SetAllPoints(button)
-
-	local r, g, b = max(random(), 0.2), max(random(), 0.2), max(random(), 0.2)
-	button.bg:SetVertexColor(r, g, b)
-	button:EnableMouseWheel(true)
-	button:SetScript('OnClick', TextureButton_OnClick)
-	button:SetScript('OnMouseWheel', TextureButton_OnMouseWheel)
-	button:SetNormalFontObject('GameFontNormalLeft')
-	button:SetHighlightFontObject('GameFontHighlightLeft')
-	return button
-end
-
-local function Panel_UpdateList(self)
-	local SML = LibStub('LibSharedMedia-3.0')
-	local textures = SML:List('statusbar')
-	local currentTexture = self.owner.sets.texture
-
+--Texture Panel
+local NUM_ITEMS, WIDTH, HEIGHT, OFFSET = 8, 150, 20, 0
+function Update_MediaButtons(self)
+	local mType = self.mediatype
+	local list = self.list
+	if not list then
+		return
+	end
+	local currentTexture = self.get(self.owner)
 	local scroll = self.scroll
-	FauxScrollFrame_Update(scroll, #textures, #self.buttons, HEIGHT + OFFSET)
-
+	FauxScrollFrame_Update(scroll, #list, #self.buttons, HEIGHT + OFFSET)
 	for i,button in pairs(self.buttons) do
 		local index = i + scroll.offset
-
-		if index <= #textures then
-			button:SetText(textures[index])
-			button.bg:SetTexture(SML:Fetch('statusbar', textures[index]))
-			button:Show()
+		if index <= #list then
+			local filePath = SML:Fetch(self.mediatype, list[index])
+			local fileName = list[index]
+			SetSelcterInfo(button, mType, filePath, fileName)
 		else
 			button:Hide()
 		end
 	end
 end
 
-local function AddTexturePanel(menu)
-	local p = menu:NewPanel(L.Texture)
-	p.UpdateList = Panel_UpdateList
-	p:SetScript('OnShow', function() p:UpdateList() end)
-	p.textures = LibStub('LibSharedMedia-3.0'):List('statusbar')
+local mediaPanel
+local function GetMediaPanel()
+	if DominosMediaPanel then
+		return DominosMediaPanel
+	end
+
+	local p = CreateFrame("Frame", "DominosMediaPanel", UIParent, "TranslucentFrameTemplate")
+	tinsert(UISpecialFrames, p:GetName())
+	p.UpdateList = Update_MediaButtons
+
+	p.close = CreateFrame("Button", p:GetName().."CloseButton", p, "UIPanelCloseButton")
+	p.close:SetPoint("TopRight", -5, -5)
+
+	p.title = p:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+	p.title:SetPoint("TopLeft", 15, -15)
+
+	p:SetScript('OnHide', function(self)
+		if p.holding then
+			p.holding:SetChecked(false)
+			p.holding:GetScript("OnShow")()
+
+			p.holding = nil
+		end
+	end)
 
 	local name = p:GetName()
 	local scroll = CreateFrame('ScrollFrame', name .. 'ScrollFrame', p, 'FauxScrollFrameTemplate')
@@ -456,23 +519,126 @@ local function AddTexturePanel(menu)
 	scroll:SetScript('OnShow', function() p.buttons[1]:SetWidth(WIDTH) end)
 	scroll:SetScript('OnHide', function() p.buttons[1]:SetWidth(WIDTH + 20) end)
 	scroll:SetPoint('TOPLEFT', 8, 0)
-	scroll:SetPoint('BOTTOMRIGHT', -24, 2)
+	scroll:SetPoint('BOTTOMRIGHT', -22, 2)
+	local scrollBar = _G[scroll:GetName() .. 'ScrollBar']
+	scrollBar:ClearAllPoints()
+	scrollBar:SetPoint("TOPLEFT", scroll, "TOPRIGHT", -6, -50)
+	scrollBar:SetPoint("BOTTOMLEFT", scroll, "BOTTOMRIGHT", -6, 26)
+	scroll:SetScript('OnMouseWheel', function(self, direction)
+		scrollBar:SetValue(scrollBar:GetValue() - direction * (scrollBar:GetHeight()/2))
+		p:UpdateList()
+	end)
+	
+	scroll:SetScript("OnMouseUp", function()
+		local buttons = p.buttons
+		for i, b in pairs(p.buttons) do
+			if MouseIsOver(b) then
+				if p.set then
+					p.set(p.owner, b:GetText())
+					p:Hide()
+				end
+			end
+		end
+	end)
 	p.scroll = scroll
 
 	--add list buttons
 	p.buttons = {}
 	for i = 1, NUM_ITEMS do
-		local b = TextureButton_Create(name .. i, p)
+		local b = CreateFrame('Button', name .. i, p)
+		b:SetWidth(WIDTH)
+		b:SetHeight(HEIGHT)
+		b:EnableMouse(false)
+		b:SetNormalFontObject('GameFontNormal')
+		b:SetHighlightFontObject('GameFontHighlight')
 		if i == 1 then
-			b:SetPoint('TOPLEFT', 4, 0)
+			b:SetPoint('TOPLEFT', 12, -37)
 		else
 			b:SetPoint('TOPLEFT', name .. i-1, 'BOTTOMLEFT', 0, -OFFSET)
-			b:SetPoint('TOPRIGHT', name .. i-1, 'BOTTOMRIGHT', 0, -OFFSET)
 		end
 		p.buttons[i] = b
 	end
 
-	p.height = 5 + (NUM_ITEMS * HEIGHT)
+	p:SetWidth(WIDTH+40)
+	p:SetHeight(50 + (NUM_ITEMS * HEIGHT))
+	return p
+end
+
+local function DisplayMedia(hold, parent, mediaType, get, set)
+	local mediaPanel = GetMediaPanel()
+
+	local holding = mediaPanel.holding
+	if holding then
+		holding:SetChecked(false)
+	end
+	if hold and (mediaPanel.holding == hold) then
+		hold:SetChecked(false)
+		mediaPanel.holding = nil
+		return mediaPanel:Hide()
+	end
+	mediaPanel.holding = hold
+	hold:SetChecked(true)
+
+	mediaPanel.owner = parent.owner
+	mediaPanel:SetParent(parent)
+	mediaPanel.title:SetText(mediaType)
+
+	mediaPanel:ClearAllPoints()
+	mediaPanel:SetPoint('TopLeft', parent:GetParent(), "TopRight")
+
+	mediaPanel.mediatype = string.lower(mediaType)
+	mediaPanel.list = SML:List(string.lower(mediaType))
+	mediaPanel.get = parent.owner[get]
+	mediaPanel.set = parent.owner[set]
+
+	mediaPanel:UpdateList()
+	mediaPanel:Show()
+end
+
+local function NewMediaButton(panel, name, mediaType, get, set)
+	local b = CreateFrame("CheckButton", panel:GetName()..name.."MediaButton", panel, "UIMenuButtonStretchTemplate")
+	b:SetScript("OnMouseDown", function()
+		DisplayMedia(b, panel, mediaType, get, set)
+	end)
+	b:SetScript("OnMouseUp", function()
+		b:GetScript("OnShow")()
+	end)
+	
+	b.texture = CreateFrame('Button', b:GetName().."Display", b)
+	b.texture:SetWidth(100)
+	b.texture:SetHeight(20)
+	b.texture:EnableMouse(false)
+	b.texture:SetNormalFontObject('GameFontNormal')
+	b.texture:SetHighlightFontObject('GameFontHighlight')
+
+	b.texture:SetPoint("Left", b, "Right")
+
+	b:SetScript("OnShow", function()
+		local mtype = string.lower(mediaType)
+		local got = panel.owner[get]
+		SetSelcterInfo(b.texture, mtype, SML:Fetch(mtype, got(panel.owner)), got(panel.owner))
+	end)
+	b:SetSize(75, 20)
+	b:SetText(name)
+	b:SetCheckedTexture(b:GetHighlightTexture())
+	
+	local prev = panel.checkbutton
+	if prev then
+		b:SetPoint('TOPLEFT', prev, 'BOTTOMLEFT', 0, -0)
+	else
+		b:SetPoint('TOPLEFT', 2, -5)
+	end
+	panel.checkbutton = b
+	panel.height = panel.height + 24
+end
+
+local function AddTexturePanel(menu)
+	local p = menu:NewPanel("Textures")
+	
+	NewMediaButton(p, "Cast Bar", "StatusBar", "GetBarTexture", "SetTexture")
+	NewMediaButton(p, "Background", "Background", "GetBackground", "SetBackground")
+
+	return p
 end
 
 local function AddLayoutPanel(menu)
@@ -483,6 +649,7 @@ local function AddLayoutPanel(menu)
 	panel:NewScaleSlider()
 	NewHeightSlider(panel)
 	NewWidthSlider(panel)
+	CheckButton(panel, "Show Icon", "showIcon")
 	return panel
 end
 
@@ -503,6 +670,9 @@ local function AddTextPanel(menu)
 	NewMenu(panel, "Time Format", "timeFormat", {"Default", "Percent", "Fraction"})
 	NewMenu(panel, "Align Text", "alignText", {"LEFT", "CENTER", "RIGHT"})
 	NewMenu(panel, "Align Time", "alignTime", {"LEFT", "CENTER", "RIGHT"})
+
+	NewMediaButton(panel, "Font", "Font", "GetFont", "SetFont")
+
 
 	return panel
 end
