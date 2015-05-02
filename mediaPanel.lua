046f2fb4b8ea76cd58043f1c60e71f15b2738798
Goranaws <goranaws@arenajunkies.com>
2015-05-02 13:46:51 -0400

    converting to Dominos style

--- mediaPanel.lua
+++ mediaPanel.lua
@@ -7,6 +7,130 @@ local Media = {}
 Addon.MediaPanel = Media
 
 local NUM_ITEMS, WIDTH, HEIGHT, OFFSET = 8, 150, 20, 0
+
+--[[ buttons ]]--
+local mediaButton = Addon:CreateClass('Button')
+do
+	function mediaButton:New(id)
+		return self:Create(id)
+	end
+
+	function mediaButton:Create(id)
+		local button = CreateFrame('Button', self:GetName() .. id, self)
+		button:SetWidth(WIDTH)
+		button:SetHeight(HEIGHT)
+		button:EnableMouse(false)
+		button:SetNormalFontObject('GameFontNormal')
+		button:SetHighlightFontObject('GameFontHighlight')
+		return button
+	end
+	
+	function mediaButton:Set(mType, filePath, fileName)
+		if mType == "hide" then
+			self:Hide()
+			return
+		end
+		self:SetText(fileName)
+		if mType == "border" then
+			self:SetBackdrop({
+				edgeFile = filePath,
+				edgeSize = 16,
+			})
+		elseif (mType == "background") or (mType == "statusbar") then
+			self:SetBackdrop({
+				bgFile = filePath,
+				tile = false,
+			})
+		end
+		if mType == "font" then
+			self:GetFontString():SetFont(filePath, 12)
+			self:SetBackdrop(nil)
+		else
+			self:GetFontString():SetFont(SML:Fetch("font", "GameFontNormal"), 12)
+		end
+		self:GetFontString():SetAllPoints(self)
+		self:Show()
+	end
+end
+
+--[[ panel ]]--
+local mediaPanel = Addon:CreateClass('Frame')
+
+
+
+
+do
+	function mediaPanel:New(name)
+		return self:Restore(name) or self:Create(name)
+	end
+
+	function mediaPanel:Create(name)
+		local panel = CreateFrame('Frame', name, UIParent, "TranslucentFrameTemplate")
+		tinsert(UISpecialFrames, panel:GetName())
+		panel.UpdateList = self.UpdateList
+		panel.close = CreateFrame("Button", panel:GetName().."CloseButton", panel, "UIPanelCloseButton")
+		panel.close:SetPoint("TopRight", -5, -5)
+		panel.title = panel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+		panel.title:SetPoint("TopLeft", 15, -15)
+		panel:SetScript('OnHide', function(self)
+			if self.holding then
+				self.holding:SetChecked(false)
+				self.holding:GetScript("OnShow")()
+				self.holding = nil
+			end
+		end)
+		
+		local scroll = CreateFrame('ScrollFrame', name .. 'ScrollFrame', panel, 'FauxScrollFrameTemplate')
+		scroll:SetScript('OnVerticalScroll', function(scroll, arg1) FauxScrollFrame_OnVerticalScroll(scroll, arg1, HEIGHT + OFFSET, function() panel:UpdateList() end) end)
+		scroll:SetScript('OnShow', function() panel.buttons[1]:SetWidth(WIDTH) end)
+		scroll:SetScript('OnHide', function() panel.buttons[1]:SetWidth(WIDTH + 20) end)
+		scroll:SetPoint('TOPLEFT', 8, 0)
+		scroll:SetPoint('BOTTOMRIGHT', -22, 2)
+
+		local scrollBar = _G[scroll:GetName() .. 'ScrollBar']
+		scrollBar:ClearAllPoints()
+		scrollBar:SetPoint("TOPLEFT", scroll, "TOPRIGHT", -6, -50)
+		scrollBar:SetPoint("BOTTOMLEFT", scroll, "BOTTOMRIGHT", -6, 26)
+
+		scroll:SetScript('OnMouseWheel', function(scroll, direction)
+			scrollBar:SetValue(scrollBar:GetValue() - direction * (scrollBar:GetHeight()/2))
+			panel:UpdateList()
+		end)
+		scroll:SetScript("OnMouseUp", function()
+			local buttons = panel.buttons
+			for i, b in pairs(panel.buttons) do
+				if MouseIsOver(b) then
+					if panel.set then
+						panel.set(panel.owner, b:GetText())
+						panel:Hide()
+					end
+				end
+			end
+		end)
+		panel.scroll = scroll
+		
+		--add list buttons
+		panel.buttons = {}
+		for i = 1, NUM_ITEMS do
+			local button = mediaButton.proto:New(i)
+			if i == 1 then
+				button:SetPoint('TOPLEFT', 12, -37)
+			else
+				button:SetPoint('TOPLEFT', name .. i-1, 'BOTTOMLEFT', 0, -OFFSET)
+			end
+			panel.buttons[i] = b
+		end
+		panel:SetWidth(WIDTH+40)
+		panel:SetHeight(50 + (NUM_ITEMS * HEIGHT))
+
+		return panel
+	end
+
+	function mediaPanel:Restore(name)
+		return _G[name]
+	end
+end
+
 function Media:UpdateList()
 	FauxScrollFrame_Update(self.scroll, #self.list, #self.buttons, HEIGHT + OFFSET)
 	for i,button in pairs(self.buttons) do
@@ -14,125 +138,44 @@ function Media:UpdateList()
 		if index <= #self.list then
 			local filePath = SML:Fetch(self.mediatype, self.list[index])
 			local fileName = self.list[index]
-			Media:SetSelectorInfo(button, self.mediatype, filePath, fileName)
+			button:Set(self.mediatype, filePath, fileName)
 		else
-			button:Hide()
+			button:Set("hide")
 		end
 	end
 end
 
-function Media:SetSelectorInfo(button, mType, filePath, fileName)
-	button:SetText(fileName)
-	if mType == "border" then
-		button:SetBackdrop({
-			edgeFile = filePath,
-			edgeSize = 16,
-		})
-	elseif (mType == "background") or (mType == "statusbar") then
-		button:SetBackdrop({
-			bgFile = filePath,
-			tile = false,
-		})
-	end
-	if mType == "font" then
-		button:GetFontString():SetFont(filePath, 12)
-		button:SetBackdrop(nil)
-	else
-		button:GetFontString():SetFont(SML:Fetch("font", "GameFontNormal"), 12)
-	end
-	button:GetFontString():SetAllPoints(button)
-	button:Show()
-end
-
 function Media:Display(anchor, clicked, mediaType, get, set)
-	local Media = self:GetPanel()
-	local holding = Media.holding
+	local panel = self:GetPanel()
+	local holding = panel.holding
 	if holding then
 		holding:SetChecked(false)
 	end
-	if clicked and (Media.holding == clicked) then
+	if clicked and (panel.holding == clicked) then
 		clicked:SetChecked(false)
-		Media.holding = nil
-		return Media:Hide()
+		panel.holding = nil
+		return panel:Hide()
 	end
-	Media.holding = clicked
+	panel.holding = clicked
 	clicked:SetChecked(true)
-	Media.owner = anchor.owner
-	Media:SetParent(anchor)
-	Media.title:SetText(mediaType)
-	Media:ClearAllPoints()
-	Media:SetPoint('TopLeft', anchor:GetParent(), "TopRight")
-	Media.mediatype = string.lower(mediaType)
-	Media.list = SML:List(string.lower(mediaType))
-	Media.get = anchor.owner[get]
-	Media.set = anchor.owner[set]
-	Media:UpdateList()
-	Media:Show()
+	panel.owner = anchor.owner
+	panel:SetParent(anchor)
+	panel.title:SetText(mediaType)
+	panel:ClearAllPoints()
+	panel:SetPoint('TopLeft', anchor:GetParent(), "TopRight")
+	panel.mediatype = string.lower(mediaType)
+	panel.list = SML:List(string.lower(mediaType))
+	panel.get = anchor.owner[get]
+	panel.set = anchor.owner[set]
+	panel:UpdateList()
+	panel:Show()
 end
 
 function Media:GetPanel()
 	if DominosMediaPanel then
 		return DominosMediaPanel
 	end
-	self.panel = CreateFrame("Frame", "DominosMediaPanel", UIParent, "TranslucentFrameTemplate")
-	tinsert(UISpecialFrames, self.panel:GetName())
-	self.panel.UpdateList = self.UpdateList
-	self.panel.close = CreateFrame("Button", self.panel:GetName().."CloseButton", self.panel, "UIPanelCloseButton")
-	self.panel.close:SetPoint("TopRight", -5, -5)
-	self.panel.title = self.panel:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-	self.panel.title:SetPoint("TopLeft", 15, -15)
-	self.panel:SetScript('OnHide', function(self)
-		if self.holding then
-			self.holding:SetChecked(false)
-			self.holding:GetScript("OnShow")()
-			self.holding = nil
-		end
-	end)
-	local name = self.panel:GetName()
-	local scroll = CreateFrame('ScrollFrame', name .. 'ScrollFrame', self.panel, 'FauxScrollFrameTemplate')
-	scroll:SetScript('OnVerticalScroll', function(scroll, arg1) FauxScrollFrame_OnVerticalScroll(scroll, arg1, HEIGHT + OFFSET, function() self.panel:UpdateList() end) end)
-	scroll:SetScript('OnShow', function() self.panel.buttons[1]:SetWidth(WIDTH) end)
-	scroll:SetScript('OnHide', function() self.panel.buttons[1]:SetWidth(WIDTH + 20) end)
-	scroll:SetPoint('TOPLEFT', 8, 0)
-	scroll:SetPoint('BOTTOMRIGHT', -22, 2)
-	local scrollBar = _G[scroll:GetName() .. 'ScrollBar']
-	scrollBar:ClearAllPoints()
-	scrollBar:SetPoint("TOPLEFT", scroll, "TOPRIGHT", -6, -50)
-	scrollBar:SetPoint("BOTTOMLEFT", scroll, "BOTTOMRIGHT", -6, 26)
-	scroll:SetScript('OnMouseWheel', function(scroll, direction)
-		scrollBar:SetValue(scrollBar:GetValue() - direction * (scrollBar:GetHeight()/2))
-		self.panel:UpdateList()
-	end)
-	scroll:SetScript("OnMouseUp", function()
-		local buttons = self.panel.buttons
-		for i, b in pairs(self.panel.buttons) do
-			if MouseIsOver(b) then
-				if self.panel.set then
-					self.panel.set(self.panel.owner, b:GetText())
-					self.panel:Hide()
-				end
-			end
-		end
-	end)
-	self.panel.scroll = scroll
-	--add list buttons
-	self.panel.buttons = {}
-	for i = 1, NUM_ITEMS do
-		local b = CreateFrame('Button', name .. i, self.panel)
-		b:SetWidth(WIDTH)
-		b:SetHeight(HEIGHT)
-		b:EnableMouse(false)
-		b:SetNormalFontObject('GameFontNormal')
-		b:SetHighlightFontObject('GameFontHighlight')
-		if i == 1 then
-			b:SetPoint('TOPLEFT', 12, -37)
-		else
-			b:SetPoint('TOPLEFT', name .. i-1, 'BOTTOMLEFT', 0, -OFFSET)
-		end
-		self.panel.buttons[i] = b
-	end
-	self.panel:SetWidth(WIDTH+40)
-	self.panel:SetHeight(50 + (NUM_ITEMS * HEIGHT))
+	self.panel = mediaPanel.proto:New("DominosMediaPanel")
 	return self.panel
 end
 
@@ -153,7 +196,7 @@ function Media:NewMediaButton(parent, name, mediaType, get, set)
 	button:SetScript("OnShow", function()
 		local mtype = string.lower(mediaType)
 		local got = parent.owner[get]
-		self:SetSelectorInfo(button.texture, mtype, SML:Fetch(mtype, got(parent.owner)), got(parent.owner))
+		(button.texture:Set(mtype, SML:Fetch(mtype, got(parent.owner)), got(parent.owner))
 		button.texture:SetWidth(abs(parent:GetWidth()-(button:GetWidth()))-5)
 	end)
 
